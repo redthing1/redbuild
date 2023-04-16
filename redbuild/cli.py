2e536a57b7f54ddbc13f85206c35a9ab31ce9f06
redthing1 <redthing1@alt.icu>
2023-04-15 18:04:52 -0700

    image and build commands

--- redbuild/cli.py
+++ redbuild/cli.py
@@ -7,6 +7,7 @@ redbuild
 import toml
 import sh
 import sys
+import os
 import typer
 import multiprocessing
 import psutil
@@ -17,6 +18,7 @@ from .engine import (
     detect_container_engine,
     get_container_engine_command,
 )
+from .util import get_builder_image_name
 
 app = typer.Typer(
     name="redbuild",
@@ -38,6 +40,103 @@ def info():
 
     container_engine = detect_container_engine()
     print(f"Container engine: {container_engine}")
+
+
+# build the build environment image
+@app.command()
+def image(
+    dockerfile: str = typer.Option(
+        "build.docker",
+        "--dockerfile",
+        "-f",
+        help="Dockerfile to use for defining the build environment",
+    ),
+    cwd: str = typer.Option(
+        ".", "--cwd", "-C", help="Directory to use as the build context"
+    ),
+):
+    # 0. get container engine
+    ctr_engine = get_container_engine_command(detect_container_engine())
+    # 1. get name for builder image
+    builder_image_name = get_builder_image_name(cwd)
+
+    # full path to dockerfile
+    dockerfile = os.path.join(cwd, dockerfile)
+
+    # 2. build builder image
+    # ensure dockerfile exists
+    if not os.path.isfile(dockerfile):
+        raise Exception(
+            f"Dockerfile {dockerfile} not found. Create it or pass a specific path with -f."
+        )
+
+    # $CONTAINER_ENGINE build -t $BUILDER_TAG $CBUILD_ARGS -f $DOCKERFILE | sed 's/^/  /'
+    print(f"Building builder image [{builder_image_name}] in context [{cwd}]:")
+    build_cmd = ctr_engine.bake(
+        "build",
+        cwd,
+        # podman build args
+        t=builder_image_name,
+        f=dockerfile,
+        # _fg=True,
+        # output formatting
+        _out=lambda line: print(f"  {line}", end=""),
+        _err=lambda line: print(f"  {line}", end=""),
+    )
+
+    build_proc = build_cmd()
+
+
+@app.command()
+def build(
+    dockerfile: str = typer.Option(
+        "build.docker",
+        "--dockerfile",
+        "-f",
+        help="Dockerfile to use for defining the build environment",
+    ),
+    buildscript: str = typer.Option(
+        "build.sh", "--buildscript", "-s", help="Build script to run in the container"
+    ),
+    cwd: str = typer.Option(
+        ".", "--cwd", "-C", help="Directory to use as the build context"
+    ),
+):
+    # 0. get container engine
+    ctr_engine = get_container_engine_command(detect_container_engine())
+
+    # 1. get name for builder image
+    builder_image_name = get_builder_image_name(cwd)
+
+    # 2. build builder image
+    image(dockerfile=dockerfile, cwd=cwd)
+
+    # 3. run build
+    # $CONTAINER_ENGINE run --rm -it -v $(pwd):/prj $CRUN_ARGS $BUILDER_TAG /bin/bash -l -c "cd /prj && $BUILDSCRIPT $ARGS" | sed 's/^/  /'
+    print(
+        f"Running build in [{builder_image_name}] with buildscript [{buildscript}] in context [{cwd}]:"
+    )
+    run_cmd = ctr_engine.bake(
+        "run",
+        # podman run args
+        "--rm",
+        # "-it",
+        "-v",
+        f"{cwd}:/prj",
+        # _fg=True,
+        # output formatting
+        _out=lambda line: print(f"  {line}", end=""),
+        _err=lambda line: print(f"  {line}", end=""),
+    )
+
+    relative_buildscript = f"./{buildscript}"
+    run_proc = run_cmd(
+        builder_image_name,
+        "/bin/bash",
+        "-l",
+        "-c",
+        f"cd /prj && {relative_buildscript}",
+    )
 
 
 @app.callback()
---
+++ redbuild/util.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import random
+import hashlib
+
+# CWD_DIRNAME=$(basename "$CWD")
+# # lowercase and alphanumeric only
+# CWD_DIRNAME=$(echo "$CWD_DIRNAME" | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:]')
+# CWD_HASH=$(echo "$CWD" | sha256sum | head -c 8)
+
+# # BUILDER_TAG=builder_$(head /dev/urandom | tr -dc a-z0-9 | head -c10)
+# BUILDER_TAG=builder_${CWD_DIRNAME}_${CWD_HASH}
+
+
+def get_builder_image_name(cwd):
+    # expand cwd to absolute path
+    cwd = os.path.abspath(cwd)
+    cwd_dirname = os.path.basename(cwd).lower()
+    cwd_dirname = "".join([c for c in cwd_dirname if c.isalnum()])
+
+    cwd_hash = hashlib.sha256(cwd.encode("utf-8")).hexdigest()[:8]
+
+    builder_tag = f"redbuild_builder_{cwd_dirname}_{cwd_hash}"
+
+    return builder_tag
